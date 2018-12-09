4f221e54b8c284c72f3452fd73fb8b96a4478ea0
Mitchell Busby <mitchbusby@gmail.com>
2018-12-09 19:19:57 +1100

    Adds Slack shiz

--- main.py
+++ main.py
@@ -13,5 +13,10 @@ def lol():
     text = request.args.get('text')
     return f'lol {text}'
 
+@app.route('/slack')
+def slack():
+    text = request.form.get('text')
+    return f'lol {text}'
+
 if __name__ == '__main__':
     app.run()
