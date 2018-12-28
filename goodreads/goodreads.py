4fa90017d8ee94c6f8af936939d5d9873ed44a72
Mitchell Busby <mitchbusby@gmail.com>
2018-12-28 23:12:41 +1100

    Hmm

--- goodreads/goodreads.py
+++ goodreads/goodreads.py
@@ -7,6 +7,14 @@ API_URL = os.environ.get('GOODREADS_URL')
 def get_random_book_from_shelf():
     response = requests.get(API_URL)
     dictionary_response = xmltodict.parse(response.text)
-    print(dictionary_response['GoodreadsResponse'].keys())
-    print(dictionary_response['GoodreadsResponse']['reviews'])
-    return dictionary_response['GoodreadsResponse']['reviews'][0]
+    # print(dictionary_response['GoodreadsResponse'].keys())
+    # print(dictionary_response['GoodreadsResponse']['reviews'])
+
+    reviews = dictionary_response['GoodreadsResponse']['reviews']
+    
+    thing = ''
+
+    for key, value in reviews:
+        thing = f'{key}, {value}'
+
+    return thing
