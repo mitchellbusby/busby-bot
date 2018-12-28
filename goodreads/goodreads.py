501954e8c881342ac7bc3978f82ea64835871827
Mitchell Busby <mitchbusby@gmail.com>
2018-12-28 23:05:22 +1100

    NFI

--- goodreads/goodreads.py
+++ goodreads/goodreads.py
@@ -7,4 +7,6 @@ API_URL = os.environ.get('GOODREADS_URL')
 def get_random_book_from_shelf():
     response = requests.get(API_URL)
     dictionary_response = xmltodict.parse(response.text)
+    print(dictionary_response['GoodreadsResponse'].keys())
+    print(dictionary_response['GoodreadsResponse']['reviews']
     return dictionary_response['GoodreadsResponse']['reviews'][0]
