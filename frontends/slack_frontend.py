7279aa52efcb095066c9357a7c20960a400b5708
Mitchell Busby <mitchbusby@gmail.com>
2018-12-30 12:53:42 +1100

    Prevent infinite loop of messages

--- frontends/slack_frontend.py
+++ frontends/slack_frontend.py
@@ -1,11 +1,32 @@
 import os
 import requests
 from flask import Blueprint, request
+from functools import lru_cache
 import traceback
 
 from goodreads import get_random_book_from_shelf
 
 SLACK_BEARER_TOKEN = os.environ.get('SLACK_BEARER_TOKEN')
+
+@lru_cache()
+def get_bot_username():
+  '''Retrieve the user information of the bot and return it. cached.'''
+  headers = {
+    'Authorization': f'Bearer {SLACK_BEARER_TOKEN}',
+    'Content-Type': 'application/json',
+  }
+  body = {
+      'token': SLACK_BEARER_TOKEN,
+    }
+  response = requests.post('https://slack.com/api/auth.test', json=body, headers=headers).json()
+  if response['ok']:
+    # use user_id to get the bot id - helpful to test if has been mentioned if
+    # more than the app_mention event is subscribed to
+    # return response['user_id']
+    return response['user']
+  else:
+    raise RuntimeError("couldn't retrieve bot info")
+
 
 def send_message(text, event_type, channel):
   '''Send a message of a particular type to a slack channel.'''
@@ -28,6 +49,10 @@ def slack_bot():
     # TODO: parse and filter and delegate responses
     event_type = request.json['event']['type']
     channel = request.json['event']['channel']
+
+    # Check if it's the bot's message - ignore it
+    if get_bot_username() == request.json['event'].get('username', ''):
+        return ''
 
     def reply(text):
       '''A reply is just a message back to the channel that messaged us.'''
