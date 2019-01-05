88f952f83ed70b8e3eabe7056591f6a5642b8001
Mitchell Busby <mitchbusby@gmail.com>
2019-01-06 09:22:56 +1100

    C# familiarity strikes again

--- bot/bot.py
+++ bot/bot.py
@@ -20,7 +20,7 @@ def handle_message(text, reply_func, state, context):
         reply_func('Sorry, I don\'t know how to tell you that yet.')
     elif matches_library_query(text):
         reply_func('Sorry, I cannot look up library books yet.')
-    else if state == STATES["REQUESTED_BOOK"]:
+    elif state == STATES["REQUESTED_BOOK"]:
         reply_func('Sorry - ask me to give you a book!')
     else:
         reply_func('G\'day! Ask me to give you a book!')
